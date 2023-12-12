24290f3442621ac845755f79ca7596ef145a535c
shahmimajid <im.shahmi@gmail.com>
2023-12-12 22:46:48 +0800

    Update nodeexporter version

--- nodeexporterInstall.sh
+++ nodeexporterInstall.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo useradd --no-create-home node_exporter
 
-wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
-tar xzf node_exporter-1.6.1.linux-amd64.tar.gz
-sudo cp node_exporter-1.6.1.linux-amd64/node_exporter /usr/local/bin/node_exporter
-cd node_exporter-1.6.1.linux-amd64
+wget https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz
+tar xzf node_exporter-1.7.0.linux-amd64.tar.gz
+sudo cp node_exporter-1.7.0.linux-amd64/node_exporter /usr/local/bin/node_exporter
+cd node_exporter-1.7.0.linux-amd64
 sudo cp node_exporter /usr/local/bin
 cd ..
-rm -rf node_exporter-1.6.1.linux-amd64.tar.gz node_exporter-1.6.1.linux-amd64
+rm -rf node_exporter-*
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
 # sudo vi /etc/systemd/system/node_exporter.service
